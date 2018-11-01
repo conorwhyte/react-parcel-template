daffb5ce579b1a52e39f1d642e0982ef7dc1279d
Conor Whyte <conor.whyte@ucdconnect.ie>
2018-11-01 23:53:25 +0000

    new files in template

--- template/src/index.js
+++ template/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, hashHistory } from 'react-router-dom';
+
+import App from './Pages/App';
+import Information from './Pages/Information'; 
+import Home from './Pages/Home';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const appElement = document.getElementById('App');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <BrowserRouter history={hashHistory}>
+    <div>
+      <App />
+      <Route exact path="/" component={Home}></Route>
+      <Route path="/info/" component={Information}></Route>
+    </div>
+  </BrowserRouter>, appElement);
